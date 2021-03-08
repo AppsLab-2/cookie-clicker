18efb9b7074f070c61486b4143afd76f34316b0e
luboslavistenik <luboslav.istenik@gmail.com>
2021-03-08 17:38:08 +0100

    Nav bar in progress

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { ClickerComponent } from './clicker/clicker.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clicker', component: ClickerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
